27caa43e4570d691d7a8ba1b9266c0b5c32cadb2
Anthony Scopatz <scopatz@gmail.com>
2020-08-04 14:41:20 -0500

    can now checkout master version

--- conda_pytorch/develop.py
+++ conda_pytorch/develop.py
@@ -1,12 +1,18 @@
 """Perform development installs of PyTorch"""
 import os
+import re
 import json
+import glob
+import tempfile
 import subprocess
+from ast import literal_eval
 
 from .tools import timed
 
 
 URL_FORMAT = "{base_url}/{platform}/{dist_name}.tar.bz2"
+SHA1_RE = re.compile("([0-9a-fA-F]{40})")
+#GIT_VERSION_RE = re.compile(r"git_version\s*=\s*['\"]([0-9a-fA-F]+)['\"]")
 
 
 @timed("Solving conda envrionment")
@@ -40,14 +46,51 @@ def deps_install(deps):
 
 @timed("Installing pytorch nightly binaries")
 def pytorch_install(url):
-    """"Install pytorch into the PWD"""
-    cmd = ["conda", "create", "--yes", "--no-deps", "--prefix", ".", url]
+    """"Install pytorch into a temporary directory"""
+    pytdir = tempfile.TemporaryDirectory()
+    cmd = ["conda", "create", "--yes", "--no-deps", "--prefix", pytdir.name, url]
     p = subprocess.run(cmd, check=True)
+    return pytdir
 
+
+def _site_packages(pytdir):
+    template = os.path.join(pytdir.name, "lib", "python*.*", "site-packages")
+    spdir = glob.glob(template)[0]
+    return spdir
+
+
+@timed("Checking out nightly PyTorch")
+def checkout_nightly_version(spdir):
+    """Get's the nightly version and then checks it out."""
+    # first get the git version from the installed module
+    version_fname = os.path.join(spdir, "torch", "version.py")
+    with open(version_fname) as f:
+        lines = f.read().splitlines()
+    for line in lines:
+        if not line.startswith('git_version'):
+            continue
+        git_version = literal_eval(line.partition("=")[2].strip())
+        break
+    else:
+        raise RuntimeError(f"Could not find git_version in {version_fname}")
+    print(f"Found released git version {git_version}")
+    # now cross refernce with nightly version
+    cmd = ["git", "show", "--no-patch", "--format=%s", git_version]
+    p = subprocess.run(cmd, capture_output=True, check=True, text=True)
+    m = SHA1_RE.search(p.stdout)
+    if m is None:
+        raise RuntimeError(f"Could not find nightly release in git history:\n  {p.stdout}")
+    nightly_version = m.group(1)
+    print(f"Found nightly release version {nightly_version}")
+    # now checkout nightly version
+    cmd = ["git", "checkout", nightly_version]
+    p = subprocess.run(cmd, check=True)
 
 
 def install():
     """Development install of PyTorch"""
     deps, pytorch = conda_solve()
     deps_install(deps)
-    pytorch_install(pytorch)
+    pytdir = pytorch_install(pytorch)
+    spdir = _site_packages(pytdir)
+    checkout_nightly_version(spdir)
